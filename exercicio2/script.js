534f77f8a7868fe43b8eda4d7cb4adca106ca845
falacomomenna <danielmenna@gmail.com>
2022-07-19 13:58:20 -0300

    Commit com a realização do Exercício 2

---
+++ exercicio2/script.js
@@ -0,0 +1,18 @@
+const usaOculos = prompt('Você usa Óculos?')
+const gostaDeRock = prompt('Você gosta de Rock?')
+const jaPrograma = prompt('Já é programador')
+
+console.log(usaOculos)
+console.log(gostaDeRock)
+console.log(jaPrograma)
+
+const pergunta1 = 'Você usa Óculos?'
+const resposta1 = prompt(pergunta1)
+const pergunta2 = 'Você gosta de Rock?'
+const resposta2 = prompt(pergunta2)
+const pergunta3 = 'Você já é programador?'
+const resposta3 = prompt(pergunta3)
+
+console.log(pergunta1, '-', resposta1)
+console.log(pergunta2, '-', resposta2)
+console.log(pergunta3, '-', resposta3)
